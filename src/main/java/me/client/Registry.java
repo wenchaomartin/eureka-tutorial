4bb717ec98e421e5f2d67b90a5cd64761d839cfe
martin <wenchaomartin@gmail.com>
2022-02-14 10:44:35 +0800

    cancel instance

--- src/main/java/me/client/Registry.java
+++ src/main/java/me/client/Registry.java
@@ -1,14 +1,34 @@
 package me.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 /**
  * @author songwenchao
  */
 public class Registry {
+
+    private static final Logger logger = LoggerFactory.getLogger(Registry.class);
+
+    private ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+
     private ConcurrentHashMap<String, ConcurrentHashMap<String, Lease<InstanceInfo>>> appName2InstanceInfo
             = new ConcurrentHashMap<>();
+
+    private Registry() {
+        initScheduleTask();
+    }
+
+    private void initScheduleTask() {
+        scheduledExecutorService.schedule();
+    }
 
     public void registry(String appName, InstanceInfo instanceInfo) {
         Map<String, Lease<InstanceInfo>> id2InstanceInfo = appName2InstanceInfo.get(appName);
@@ -23,4 +43,23 @@ public class Registry {
             id2InstanceInfo.put(instanceInfo.getInstanceId(), lease);
         }
     }
+
+    public void cancel(String appName, String instanceId) {
+        Map<String, Lease<InstanceInfo>> existId2InstanceInfoLease = appName2InstanceInfo.get(appName);
+        if (existId2InstanceInfoLease != null) {
+            logger.error("appName's instance not exists {}", appName);
+        }
+
+        existId2InstanceInfoLease.remove(instanceId);
+
+    }
+
+    private void evictExpiredLease() {
+        Set<Map.Entry<String, ConcurrentHashMap<String, Lease<InstanceInfo>>>> entrySet
+                = appName2InstanceInfo.entrySet();
+     
+        Iterator<Map.Entry<String, ConcurrentHashMap<String, Lease<InstanceInfo>>>> iterator = entrySet.iterator();
+    }
+
+
 }
